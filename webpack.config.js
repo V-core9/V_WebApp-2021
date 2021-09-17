927a27b3a0134a9c05e9c7847aceadceda51b8d0
⩹⌬⩺ <slavko.vuletic92@gmail.com>
2021-09-17 14:48:08 +0200

    yea

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,21 @@
 const path = require("path");
 
 module.exports = {
-  entry: "./src/app.js",
+  entry: {
+    app: "./src/app.js",
+    //v_cursor: './[.v.]_dev_apps/_x1/SRC/helpers/v_cursor.js',
+  },
+  target: "web",
   output: {
-    filename: "app_v-pack.js",
     path: path.resolve(__dirname, "dist"),
+    filename: "[name].v_pack.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
   },
 };
