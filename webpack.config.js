3f1e9c27e7c8eaf722bb3e600596882b66b45e23
⩹⌬⩺ <slavko.vuletic92@gmail.com>
2021-10-03 22:05:15 +0200

    yeaaaa

--- webpack.config.js
+++ webpack.config.js
@@ -6,11 +6,13 @@ module.exports = {
   mode: mode,
   entry: {
     app_root: "./SOURCE/App/app_root.js",
+    V_DisplayDriver: "./SOURCE/v_modules/v_display_driver/displayDriver.js",
+    V_DomPrinter: "./SOURCE/v_modules/v_dom_printer/domPrinter.js",
   },
   target: vCfg.target,
   output: {
     path: path.resolve(__dirname, "PUBLIC"),
-    filename: "[name].v_pack.js",
+    filename: "[name].V-core9.js",
   },
   module: {
     rules: [
