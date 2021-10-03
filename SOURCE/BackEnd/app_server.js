7fbf6dfc77ef2fc54a1105ed9ff2c7bbf3215771
⩹⌬⩺ <slavko.vuletic92@gmail.com>
2021-10-03 19:36:57 +0200

    yea

--- SOURCE/BackEnd/app_server.js
+++ SOURCE/BackEnd/app_server.js
@@ -2,6 +2,7 @@
 const path = require("path");
 
 // Setup basic express server
+var compression = require('compression')
 const express = require("express");
 const app = express();
 const server = require("http").createServer(app);
@@ -11,9 +12,16 @@ const appConfig = require("../../__appConfig");
 
 console.log(appConfig);
 
+
 server.listen(appConfig.port, () => {
   console.log("Server listening at appConfig.port %d", appConfig.port);
 });
 
+
+// compress all responses
+app.use(compression())
+
+
 // Routing
-app.use(express.static(path.join(__dirname, "dist")));
+app.use(express.static(path.join(__dirname, "../../PUBLIC")));
+
