964138c8fbef0984886d3401e0a0e3b348c3e4ed
⩹⌬⩺ <slavko.vuletic92@gmail.com>
2021-10-05 06:52:55 +0200

    simple routing for the express.js in the backedn folder + few minor things

--- SOURCE/BackEnd/app_server.js
+++ SOURCE/BackEnd/app_server.js
@@ -1,5 +1,6 @@
 
 const path = require("path");
+let fs = require("fs")
 
 // Setup basic express server
 var compression = require('compression')
@@ -9,26 +10,33 @@ const server = require("http").createServer(app);
 
 // Load Configuration
 const appConfig = require("../../__appConfig");
-
 console.log(appConfig);
-
 
 server.listen(appConfig.port, () => {
   console.log("Server listening at appConfig.port %d", appConfig.port);
 });
 
-
 // compress all responses
 app.use(compression())
 
-
-// Routing
+// Routing to the PUBLIC folder...getting static content available to web
 app.use(express.static(path.join(__dirname, "../../PUBLIC")));
 
-app.get('/hello', function (req, res) {
-  res.send('hello world');
-})
 
+// Get page by slug
+app.get('/Vadmin', function (req, res) {
+  var page = fs.readFileSync(path.join(__dirname, "../../PUBLIC/index.html"));
+  var pageInfo = '<script>' + JSON.stringify({ appConfig: appConfig, page_slug: req.params.page_slug, response_timestamp: Date.now() }, true, 2) + '</script>';
+  res.send(pageInfo + "ADMIN PAGE" + page);
+});
+
+
+//-> PUBLIC 
+
+// Get page by slug
 app.get('/:page_slug', function (req, res) {
-  res.send('page_slug  >>  '+ req.params.page_slug);
-})
+  var page = fs.readFileSync(path.join(__dirname, "../../PUBLIC/index.html"));
+  var pageInfo = '<script>' + JSON.stringify({ appConfig: appConfig, page_slug: req.params.page_slug, response_timestamp: Date.now() }, true, 2) + '</script>';
+  res.send(pageInfo + page);
+});
+
